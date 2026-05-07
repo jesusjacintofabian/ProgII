09b71e02cb1e830b7f75ff5f3c16dcbb552247c5
Jesus Jacinto Fabian <jesusjacintofabian@gmail.com>
2026-05-06 20:47:00 -0500

    Lab6 Terminado

---
+++ Lab6/Lab622.py
@@ -0,0 +1,14 @@
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+#retorna en formato: año, mes, día, hora, minuto, segundo y microsegundo.
+
+print(x.year)
+print(x.strftime("%A"))
+#imprime año y dia de semana en ingles
+
+y = datetime.datetime(2010, 5, 11)
+print(y)
+#crear fecha personalizada con formado año, mes, dia
